246d2eb1f5012b11cc3062c564385dc69ba861ea
Kangs <>
2020-05-01 16:12:47 -0700

    Simple calcular show cases test coverage

---
+++ src/main/java/com/kavinschool/calc/Calc.java
@@ -0,0 +1,39 @@
+package com.kavinschool.calc;
+
+public class Calc {
+	private double x, y;
+
+	Calc(double a, double b) {
+		x = a;
+		y = b;
+	}
+
+	public double add() {
+		return x + y;
+	}
+
+	public double mul() {
+		return x * y;
+	}
+
+	public double sub() {
+		return x - y;
+	}
+
+	public double div() {
+		return x / y;
+	}
+
+	public double rem() {
+		return x % y;
+	}
+
+	public double cube() {
+		return x * x * x;
+	}
+
+	public double square() {
+		return x * x ;
+	}
+
+}
